b57dc8ca4251fffd476c3843b65f2697fc06f86d
dylandoyle11 <44929509+dylandoyle11@users.noreply.github.com>
2019-05-16 15:10:38 -0400

    Update Candidate.py

--- Candidate.py
+++ Candidate.py
@@ -1,1 +1,11 @@
+class Candidate:
 
+    def __init__(self, handler, handler_email, name, l_name, end):
+        self.handler = handler
+        self.name = name
+        self.l_name = l_name
+        self.handler_email = handler_email
+        self.end = end
+
+    def __str__(self):
+        return str(self.handler)
