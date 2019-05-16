bf8bc2506775b3740f1db0e4c4fb7513f9fe9e58
dylandoyle11 <44929509+dylandoyle11@users.noreply.github.com>
2019-05-16 17:06:38 -0400

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -1,4 +1,13 @@
 class Employee:
+    "" Class to represent an employee
+    
+    Attributes
+    
+    name: str
+        Name of employee
+    email: str
+        Email of employee
+    """
 
     def __init__(self, name, email):
         self.name = name
