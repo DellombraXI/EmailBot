6254fbcca336ea880d75e71eb30e79afc552bc19
dylandoyle11 <44929509+dylandoyle11@users.noreply.github.com>
2019-05-16 17:05:22 -0400

    Update Candidate.py

--- Candidate.py
+++ Candidate.py
@@ -1,4 +1,19 @@
 class Candidate:
+    """ Class to respresent a candidate and is assigned when the end date is within 2 days
+    
+    Attributes
+    
+    handler: str
+        Name of the employee associated with the candidate
+    handler_email: str
+        Email of the employee associated with the candidate
+    name: str
+        First name of candidate
+    l_name: str
+        Last name of candidate
+    end: int
+        End date in # of days
+    """
 
     def __init__(self, handler, handler_email, name, l_name, end):
         self.handler = handler
